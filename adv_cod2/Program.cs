448ad58f9bb4534795b89b5a6a2e6f0f06d87747
geigerkira@stud.uni-obuda.hu <geigerkira@stud.uni-obuda.hu>
2023-12-02 19:34:51 +0100

    day2_2 finished

--- adv_cod2/Program.cs
+++ adv_cod2/Program.cs
@@ -13,13 +13,20 @@ namespace adv_cod2
             string[] lines = System.IO.File.ReadAllLines("input.txt");
 
             List<int> ids = new List<int>();
+            List<int> mins = new List<int>();
 
             bool possible;
 
             int piece;
+            int red;
+            int green;
+            int blue;
 
             foreach (var line in lines)
             {
+                red = 0;
+                green = 0;
+                blue = 0;
                 possible = true;
 
                 int id = int.Parse((line.Split(':')[0]).Split(' ')[1]);
@@ -39,8 +46,23 @@ namespace adv_cod2
                         {
                             possible = false;
                         }
+
+                        if (color.Contains("red") && piece > red)
+                        {
+                            red = piece;
+                        }
+                        else if (color.Contains("green") && piece > green)
+                        {
+                            green = piece;
+                        }
+                        else if (color.Contains("blue") && piece > blue)
+                        {
+                            blue = piece;
+                        }
                     }
                 }
+
+                mins.Add(red * green * blue);
 
                 if (possible)
                 {
@@ -49,6 +71,7 @@ namespace adv_cod2
             }
 
             Console.WriteLine(ids.Sum());
+            Console.WriteLine(mins.Sum());
             Console.ReadLine();
         }
     }
