e6c157e7532a5955b78bdaa9510d5c207dc2709c
geigerkira@stud.uni-obuda.hu <geigerkira@stud.uni-obuda.hu>
2023-12-04 09:52:24 +0100

    day4_1 finished

---
+++ adv_cod4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adv_cod4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+            List<int> points = new List<int>();
+
+            string inputs;
+
+            foreach (var line in lines)
+            {
+                inputs = line.Split(':')[1];
+                points.Add(countPoints(inputs));
+            }
+
+            Console.WriteLine(points.Sum());
+            Console.ReadLine();
+        }
+
+        static int countPoints(string inputs)
+        {
+            int point = 0;
+            bool firstMatch = true;
+            string[] winningNums = (inputs.Split('|')[0]).Split(' ');
+            string[] ownNums = (inputs.Split('|')[1]).Split(' ');
+
+            foreach (var wNum in winningNums)
+            {
+                if (ownNums.Contains(wNum) && wNum != "")
+                {
+                    if (firstMatch)
+                    {
+                        point += 1;
+                        firstMatch = false;
+                    }
+                    else
+                    {
+                        point = point * 2;
+                    }
+                }
+            }
+
+            return point;
+        }
+    }
+}
